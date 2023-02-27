f1377c11018be4d7ac579cf4ee7a60fefb21a907
Quentin Moss <quentin.moss@protonmail.com>
2023-02-26 16:08:42 -0800

    step 3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::net::TcpListener;
-use std::io::Write;
+use std::io::{Write, Read};
 
 // Client will only send PING for now, if we receive a PING, respond with PONG
 // Takes zero or one arguments
@@ -17,17 +17,27 @@ use std::io::Write;
 // For Arrays, the first byte of the reply is "*"
 
 fn handle_client(mut stream: std::net::TcpStream) {
-   stream.write(b"+PONG\r\n");
+    let mut buf = [0; 512];
+
+    loop {
+        let bytes_read = stream.read(&mut buf).unwrap();
+        if bytes_read == 0 {
+            println!("Client closed connection?");
+            break;
+        } 
+
+        stream.write(b"+PONG\r\n").unwrap();
+    }
 
 }
+
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                println!("accepted new connection");
-                handle_client(stream);
+                handle_client(stream);   
             }
             Err(e) => {
                 println!("error: {}", e);
